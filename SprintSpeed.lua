d1567e6719236985bfda4280c5eb2e0efb797895
dollarxxxxxxxxxx <65142289+dollarxxxxxxxxxx@users.noreply.github.com>
2020-09-01 17:05:01 -0300

    Create SprintSpeed.lua

---
+++ SprintSpeed.lua
@@ -0,0 +1,15 @@
+repeat wait() until game.Players.LocalPlayer
+local Mouse = game.Players.LocalPlayer:GetMouse()
+local Plr = game.Players.LocalPlayer
+ 
+Mouse.KeyDown:connect(function(KeyDown)
+if KeyDown == "0" then
+Plr.Character.Humanoid.WalkSpeed = 190
+end
+end)
+ 
+Mouse.KeyUp:connect(function(KeyUp)
+if KeyUp == "0" then
+Plr.Character.Humanoid.WalkSpeed = 16
+end
+end)
