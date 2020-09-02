47eae15e1863619e8946e730e1f6a7a31bdf45b9
dollarxxxxxxxxxx <65142289+dollarxxxxxxxxxx@users.noreply.github.com>
2020-09-02 14:23:39 -0300

    Update Gunanims.lua

--- Gunanims.lua
+++ Gunanims.lua
@@ -4,7 +4,7 @@ if (string.lower(msg) == "+gunanims") then
 local lplr = game:GetService("Players").LocalPlayer
 local shotty = lplr.Backpack:FindFirstChild("Shotty")
 local Animation = Instance.new("Animation")
-Animation.AnimationId = "rbxassetid://457896265"
+Animation.AnimationId = "rbxassetid://889968874"
 local plr = game:GetService('Players').LocalPlayer
 local HUM = plr.Character.Humanoid:LoadAnimation(Animation)
 shotty.Equipped:connect(function()
@@ -18,7 +18,7 @@ end)
 local lplr = game:GetService("Players").LocalPlayer
 local Glock = lplr.Backpack:FindFirstChild("Glock")
 local Animation = Instance.new("Animation")
-Animation.AnimationId = "rbxassetid://457896265"
+Animation.AnimationId = "rbxassetid://889968874"
 local plr = game:GetService('Players').LocalPlayer
 local HUM = plr.Character.Humanoid:LoadAnimation(Animation)
 Glock.Equipped:connect(function()
